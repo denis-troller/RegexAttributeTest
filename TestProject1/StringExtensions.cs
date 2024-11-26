fed48cdae39afb98ba43f7aa1cb31cd2265feca4
denis.troller <denis.troller@sonarsource.com>
2024-11-26 21:02:11 +0100

    Add project files.

---
+++ TestProject1/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace TestProject1
+{
+    internal static partial class StringExtensions
+    {
+        public static string RemoveWhitespaces(this string s)
+        {
+            return WhitespaceRegex().Replace(s, string.Empty);
+        }
+
+        [GeneratedRegex(@"\s")]
+        private static partial Regex WhitespaceRegex();
+    }
+
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1 : PageTest
+    {
+        [TestMethod]
+        public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+        {
+
+        }
+    }
+}
